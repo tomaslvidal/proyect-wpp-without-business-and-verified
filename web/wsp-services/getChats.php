275e6a44ab024649021867409b0dc8d7571609f9
cheqeeh <vidaltomas08@gmail.com>
2022-06-30 14:28:06 -0300

    up

--- web/wsp-services/getChats.php
+++ web/wsp-services/getChats.php
@@ -2,18 +2,18 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'http://nodesv1.eviajes.online:9595/chat/sendmessage/5491151138878',
+  CURLOPT_URL => 'http://nodesv1.eviajes.online:9595/chat/getchats',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => 'message=*Online* '.date('d/m/Y H:i:s').'',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/x-www-form-urlencoded'
-  ),
+//   CURLOPT_CUSTOMREQUEST => 'POST',
+//   CURLOPT_POSTFIELDS => 'message=*Online* '.date('d/m/Y H:i:s').'',
+//   CURLOPT_HTTPHEADER => array(
+//     'Content-Type: application/x-www-form-urlencoded'
+//   ),
 ));
 
 $response = curl_exec($curl);
